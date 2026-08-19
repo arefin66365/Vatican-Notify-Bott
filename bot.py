784ef49b193c95ef62e118df0a69e17e49a5fc51
arefin66365 <100435505+arefin66365@users.noreply.github.com>
2026-08-19 18:01:49 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -5,13 +5,13 @@ from telegram import Bot
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+bot = Bot(token=TOKEN)
+
 
 async def send_message():
-    bot = Bot(token=TOKEN)
-
     await bot.send_message(
         chat_id=CHAT_ID,
-        text="✅ Vatican Notify Bot is working!"
+        text="✅ Vatican Bot is working!"
     )
 
 
