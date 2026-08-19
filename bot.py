8e87d71840d9250680c18e9a5ee3b859cacb6bc7
arefin66365 <100435505+arefin66365@users.noreply.github.com>
2026-08-19 17:23:14 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
-print("Vatican Bot Started")
+import os
+import asyncio
+from telegram import Bot
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+
+async def send_message():
+    bot = Bot(token=TOKEN)
+
+    await bot.send_message(
+        chat_id=CHAT_ID,
+        text="✅ Vatican Notify Bot is working!"
+    )
+
+
+asyncio.run(send_message())
