b444ff987ad67d8f2f4227a4384310821d00272b
arefin66365 <100435505+arefin66365@users.noreply.github.com>
2026-08-19 22:43:29 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 from telegram import Bot
+from playwright.async_api import async_playwright
 
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
@@ -8,11 +9,35 @@ CHAT_ID = os.getenv("CHAT_ID")
 bot = Bot(token=TOKEN)
 
 
-async def send_message():
+async def send_message(text):
     await bot.send_message(
         chat_id=CHAT_ID,
-        text="✅ Vatican Bot is working!"
+        text=text
     )
 
 
-asyncio.run(send_message())
+async def check_vatican():
+
+    async with async_playwright() as p:
+
+        browser = await p.chromium.launch(
+            headless=True
+        )
+
+        page = await browser.new_page()
+
+        await page.goto(
+            "https://tickets.museivaticani.va/home",
+            wait_until="networkidle"
+        )
+
+        title = await page.title()
+
+        await send_message(
+            f"🌐 Vatican page opened\nTitle: {title}"
+        )
+
+        await browser.close()
+
+
+asyncio.run(check_vatican())
